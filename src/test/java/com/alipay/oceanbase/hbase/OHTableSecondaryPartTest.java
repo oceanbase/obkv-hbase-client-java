278b82c9f17f11d300ba7cea1d851d9cae9ce1e7
vanson <43193589+WeiXinChan@users.noreply.github.com>
2025-02-28 15:11:18 +0800

    update tests (#160)
    (cherry picked from commit 0fad7f747e9e47022ff8a9fe00feddc83501e1f6)

--- src/test/java/com/alipay/oceanbase/hbase/OHTableSecondaryPartTest.java
+++ src/test/java/com/alipay/oceanbase/hbase/OHTableSecondaryPartTest.java
@@ -18,8 +18,9 @@
 package com.alipay.oceanbase.hbase;
 
 import com.alipay.oceanbase.hbase.util.ObHTableTestUtil;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.CompareFilter;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -42,7 +43,7 @@ public class OHTableSecondaryPartTest {
             "  `V` varbinary(1024) DEFAULT NULL," +
             "  `G` bigint(20) GENERATED ALWAYS AS (-T)," +
             "  PRIMARY KEY (`K`, `Q`, `T`)" +
-            ") PARTITION BY RANGE COLUMNS(`G`) SUBPARTITION BY KEY(`K`) SUBPARTITIONS 97 (" +
+            ") PARTITION BY RANGE COLUMNS(`G`) SUBPARTITION BY KEY(`K`) SUBPARTITIONS 3 (" +
             "  PARTITION `p0` VALUES LESS THAN (1728921600000)," +
             "  PARTITION `p1` VALUES LESS THAN (1729008000000)," +
             "  PARTITION `p2` VALUES LESS THAN (1729094400000)," +
@@ -56,7 +57,7 @@ public class OHTableSecondaryPartTest {
             "  `G` bigint(20) GENERATED ALWAYS AS (-T)," +
             "  K_PREFIX varbinary(1024) generated always as (substring(`K`, 1, 4))," +
             "  PRIMARY KEY (`K`, `Q`, `T`)" +
-            ") PARTITION BY RANGE COLUMNS(`G`) SUBPARTITION BY KEY(`K_PREFIX`) SUBPARTITIONS 97 (" +
+            ") PARTITION BY RANGE COLUMNS(`G`) SUBPARTITION BY KEY(`K_PREFIX`) SUBPARTITIONS 3 (" +
             "  PARTITION `p0` VALUES LESS THAN (1728921600000)," +
             "  PARTITION `p1` VALUES LESS THAN (1729008000000)," +
             "  PARTITION `p2` VALUES LESS THAN (1729094400000)," +
@@ -74,7 +75,7 @@ public class OHTableSecondaryPartTest {
             "    SUBPARTITION `p1` VALUES LESS THAN (1729008000000)," +
             "    SUBPARTITION `p2` VALUES LESS THAN (1729094400000)," +
             "    SUBPARTITION `p3` VALUES LESS THAN MAXVALUE" +
-            ") PARTITIONS 97;";
+            ") PARTITIONS 3;";
     private static String createTableStmt4 = "CREATE TABLE IF NOT EXISTS `" + tableNames[3] + "` (" +
             "  `K` varbinary(1024) NOT NULL," +
             "  `Q` varbinary(1024) NOT NULL," +
@@ -88,7 +89,7 @@ public class OHTableSecondaryPartTest {
             "    SUBPARTITION `p1` VALUES LESS THAN (1729008000000)," +
             "    SUBPARTITION `p2` VALUES LESS THAN (1729094400000)," +
             "    SUBPARTITION `p3` VALUES LESS THAN MAXVALUE" +
-            ") PARTITIONS 97;";
+            ") PARTITIONS 3;";
     private static String createTableStmts[] = new String[] {createTableStmt1, createTableStmt2, createTableStmt3, createTableStmt4};
 
     public static void dropTables() throws Exception {
@@ -96,6 +97,7 @@ public class OHTableSecondaryPartTest {
         for (int i = 0; i < tableNames.length; i++) {
             String stmt = "DROP TABLE IF EXISTS " + tableNames[i] + ";";
             conn.createStatement().execute(stmt);
+            System.out.println("drop table " + tableNames[i] + " done");
         }
     }
 
@@ -103,6 +105,7 @@ public class OHTableSecondaryPartTest {
         Connection conn = ObHTableTestUtil.getConnection();
         for (int i = 0; i < createTableStmts.length; i++) {
             conn.createStatement().execute(createTableStmts[i]);
+            System.out.println("create table " + tableNames[i] + " done");
         }
     }
 
@@ -111,6 +114,7 @@ public class OHTableSecondaryPartTest {
         for (int i = 0; i < tableNames.length; i++) {
             String stmt = "TRUNCATE TABLE " + tableNames[i] + ";";
             conn.createStatement().execute(stmt);
+            System.out.println("truncate table " + tableNames[i] + " done");
         }
     }
 
@@ -161,14 +165,91 @@ public class OHTableSecondaryPartTest {
             OHTableClient hTable = ObHTableTestUtil.newOHTableClient(getTableName(tableNames[i]));
             hTable.init();
 
+            System.out.println("put table " + tableNames[i] + " begin");
             String family = getColumnFamilyName(tableNames[i]);
             String key = "putKey";
-            String column1 = "putColumn";
+            String column = "putColumn";
             String value = "value";
             long timestamp = System.currentTimeMillis();
             Put put = new Put(toBytes(key));
-            put.add(family.getBytes(), column1.getBytes(), timestamp, toBytes(value));
+            put.add(family.getBytes(), column.getBytes(), timestamp, toBytes(value));
             hTable.put(put);
+            System.out.println("put table " + tableNames[i] + " done");
+
+            hTable.close();
+        }
+    }
+
+    @Test
+    public void testIncrement() throws Exception {
+        for (int i = 0; i < tableNames.length; i++) {
+            OHTableClient hTable = ObHTableTestUtil.newOHTableClient(getTableName(tableNames[i]));
+            hTable.init();
+
+            System.out.println("increment table " + tableNames[i] + " begin");
+            String family = getColumnFamilyName(tableNames[i]);
+            String key = "Key";
+            String column1 = "Column1";
+            String column2 = "Column2";
+            Increment increment = new Increment(key.getBytes());
+            increment.addColumn(family.getBytes(), column1.getBytes(), 1L);
+            increment.addColumn(family.getBytes(), column2.getBytes(), 1L);
+            hTable.increment(increment);
+            System.out.println("increment table " + tableNames[i] + " done");
+
+            hTable.close();
+        }
+    }
+
+    @Test
+    public void testAppend() throws Exception {
+        for (int i = 0; i < tableNames.length; i++) {
+            OHTableClient hTable = ObHTableTestUtil.newOHTableClient(getTableName(tableNames[i]));
+            hTable.init();
+
+            System.out.println("append table " + tableNames[i] + " begin");
+            String family = getColumnFamilyName(tableNames[i]);
+            String key = "Key";
+            String column1 = "Column1";
+            String column2 = "Column2";
+            String value = "app";
+            Append append = new Append(key.getBytes());
+            KeyValue kv1 = new KeyValue(key.getBytes(), family.getBytes(), column1.getBytes(), value.getBytes());
+            KeyValue kv2 = new KeyValue(key.getBytes(), family.getBytes(), column2.getBytes(), value.getBytes());
+            append.add(kv1);
+            append.add(kv2);
+            hTable.append(append);
+            System.out.println("append table " + tableNames[i] + " done");
+
+            hTable.close();
+        }
+    }
+
+    @Test
+    public void testCheckAndMutate() throws Exception {
+        for (int i = 0; i < tableNames.length; i++) {
+            OHTableClient hTable = ObHTableTestUtil.newOHTableClient(getTableName(tableNames[i]));
+            hTable.init();
+
+            System.out.println("checkAndMutate table " + tableNames[i] + " begin");
+            String family = getColumnFamilyName(tableNames[i]);
+            String key = "putKey";
+            String column = "putColumn";
+            String value = "value";
+            String newValue = "newValue";
+            RowMutations mutations = new RowMutations(key.getBytes());
+            Put put = new Put(key.getBytes());
+            long timestamp = System.currentTimeMillis();
+            put.add(family.getBytes(), column.getBytes(), timestamp, toBytes(value));
+            hTable.put(put);
+
+            Put newPut = new Put(key.getBytes());
+            newPut.add(family.getBytes(), column.getBytes(), timestamp, toBytes(newValue));
+            mutations.add(newPut);
+            hTable.checkAndMutate(key.getBytes(), family.getBytes(),
+                    column.getBytes(), CompareFilter.CompareOp.EQUAL, value.getBytes(),
+                    mutations);
+            System.out.println("checkAndMutate table " + tableNames[i] + " done");
 
             hTable.close();
         }
