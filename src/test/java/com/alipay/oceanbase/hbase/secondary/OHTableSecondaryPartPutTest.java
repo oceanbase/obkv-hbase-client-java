362590be2fd2d6b30a741dd3947b8c7e1045ddf0
maochongxin <maochongxin@gmail.com>
2025-03-14 18:46:20 +0800

    put test case
    (cherry picked from commit 304c1656b39dabb9ecdab2286506b3c0f2916e8d)

--- src/test/java/com/alipay/oceanbase/hbase/secondary/OHTableSecondaryPartPutTest.java
+++ src/test/java/com/alipay/oceanbase/hbase/secondary/OHTableSecondaryPartPutTest.java
@@ -21,19 +21,17 @@ package com.alipay.oceanbase.hbase.secondary;
 import com.alipay.oceanbase.hbase.OHTableClient;
 import com.alipay.oceanbase.hbase.util.ObHTableTestUtil;
 import com.alipay.oceanbase.hbase.util.TableTemplateManager;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Put;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Pair;
+import org.junit.*;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.alipay.oceanbase.hbase.util.ObHTableSecondaryPartUtil.*;
-import static com.alipay.oceanbase.hbase.util.ObHTableTestUtil.FOR_EACH;
+import static com.alipay.oceanbase.hbase.util.ObHTableTestUtil.*;
 import static org.apache.hadoop.hbase.util.Bytes.toBytes;
 
 public class OHTableSecondaryPartPutTest {
@@ -45,7 +43,9 @@ public class OHTableSecondaryPartPutTest {
     public static void before() throws Exception {
         openDistributedExecute();
         for (TableTemplateManager.TableType type : TableTemplateManager.TableType.values()) {
-            createTables(type, tableNames, group2tableNames, true);
+            if (!type.name().contains("TIME")) {
+                createTables(type, tableNames, group2tableNames, true);
+            }
         }
     }
 
@@ -66,43 +66,271 @@ public class OHTableSecondaryPartPutTest {
 
         String family = getColumnFamilyName(tableName);
         String key = "putKey";
-        String column = "putColumn";
         String column1 = "putColumn1";
+        String column2 = "putColumn2";
         String value = "value";
-        long timestamp = System.currentTimeMillis();
-        Put put = new Put(toBytes(key));
-        put.add(family.getBytes(), column.getBytes(), timestamp, toBytes(value));
-        put.add(family.getBytes(), column1.getBytes(), timestamp, toBytes(value));
-        hTable.put(put);
 
+        { // put new key and get
+            Put put = new Put(toBytes(key));
+            put.add(family.getBytes(), column1.getBytes(), toBytes(column1 + value));
+            put.add(family.getBytes(), column2.getBytes(), toBytes(column2 + value));
+            hTable.put(put);
+            
+            Get get = new Get(toBytes(key));
+            get.addColumn(family.getBytes(), column1.getBytes());
+            get.addColumn(family.getBytes(), column2.getBytes());
+            Result r = hTable.get(get);
+            Assert(tableName, ()->Assert.assertEquals(2, r.size()));
+            Assert(tableName, ()->Assert.assertTrue(ObHTableTestUtil.secureCompare((column1 + value).getBytes(), r.getValue(family.getBytes(), column1.getBytes()))));
+            Assert(tableName, ()->Assert.assertTrue(ObHTableTestUtil.secureCompare((column1 + value).getBytes(), r.getValue(family.getBytes(), column1.getBytes()))));
+        }
+
+        { // put exist key and get
+            long timestamp = System.currentTimeMillis();
+            Put put = new Put(toBytes(key));
+            put.add(family.getBytes(), column1.getBytes(), toBytes(column1 + value + timestamp));
+            hTable.put(put);
+            
+            Get get = new Get(toBytes(key));
+            get.addColumn(family.getBytes(), column1.getBytes());
+            Result r = hTable.get(get);
+            Assert(tableName, ()->Assert.assertEquals(1, r.size()));
+            Assert(tableName, ()->Assert.assertTrue(ObHTableTestUtil.secureCompare(toBytes(column1 + value + timestamp), r.getValue(family.getBytes(), column1.getBytes()))));
+        }
+
+        { // test timestamp update
+            long timestamp = System.currentTimeMillis();
+            Put put = new Put(toBytes(key));
+            put.add(family.getBytes(), column1.getBytes(), timestamp, toBytes(column1 + value + timestamp));
+            hTable.put(put);
+            
+            Get get = new Get(toBytes(key));
+            get.addColumn(family.getBytes(), column1.getBytes());
+            Result r = hTable.get(get);
+            Assert(tableName, ()->Assert.assertEquals(1, r.size()));
+            Assert(tableName, ()->Assert.assertTrue(ObHTableTestUtil.secureCompare(toBytes(column1 + value + timestamp), r.getValue(family.getBytes(), column1.getBytes()))));
+            Assert(tableName, ()->Assert.assertEquals(timestamp, r.rawCells()[0].getTimestamp()));
+            
+            Put put1 = new Put(toBytes(key));
+            put1.add(family.getBytes(), column1.getBytes(), toBytes(column1 + value));
+            hTable.put(put1);
+            
+            Result r2 = hTable.get(get);
+            Assert(tableName, ()->Assert.assertEquals(1, r2.size()));
+            Assert(tableName, ()->Assert.assertTrue(ObHTableTestUtil.secureCompare(toBytes(column1 + value), r2.getValue(family.getBytes(), column1.getBytes()))));
+            Assert(tableName, ()->Assert.assertTrue(timestamp < r2.rawCells()[0].getTimestamp()));
+        }
+
+       
+        
+        hTable.close();
+    }
+    
+    public static void testBatchPutImpl(String tableName) throws Exception {
+        OHTableClient hTable = ObHTableTestUtil.newOHTableClient(getTableName(tableName));
+        hTable.init();
+        String family = getColumnFamilyName(tableName);
+        String key = "putKey";
+        String column1 = "putColumn1";
+        String column2 = "putColumn2";
+        String value = "value";
+        {
+            long timestamp = System.currentTimeMillis();
+            List<Put> puts = new ArrayList<>();
+            Get get = new Get(toBytes(key));
+            for (int i = 0; i < 10; ++i) {
+                Put put = new Put(toBytes(key));
+                put.add(family.getBytes(), column1.getBytes(), timestamp, toBytes(column1 + value + timestamp + i));
+                put.add(family.getBytes(), column2.getBytes(), timestamp, toBytes(column2 + value + timestamp + i));
+                puts.add(put);
+                get.addColumn(family.getBytes(), column1.getBytes());
+                get.addColumn(family.getBytes(), column2.getBytes());
+            }
+
+            hTable.batch(puts);
+            Result result = hTable.get(get);
+            Assert(tableName, ()->Assert.assertEquals(2, result.size()));
+            for (Cell cell : result.rawCells()) {
+                Assert(tableName, ()->Assert.assertEquals(timestamp, cell.getTimestamp()));
+            }
+            Assert(tableName, () -> Assert.assertTrue(secureCompare((column1 + value + timestamp + 9).getBytes(), result.getValue(family.getBytes(), column1.getBytes()))));
+        }
+        {
+            long timestamp = System.currentTimeMillis();
+            List<Put> puts = new ArrayList<>();
+            List<Get> gets = new ArrayList<>();
+            
+            for (int i = 0; i < 10; ++i) {
+                Put put = new Put(toBytes(key + i));
+                put.add(family.getBytes(), column1.getBytes(), timestamp, toBytes(column1 + value + timestamp + i));
+                put.add(family.getBytes(), column2.getBytes(), timestamp, toBytes(column2 + value + timestamp + i));
+                puts.add(put);
+                Get get = new Get(toBytes(key + i));
+                get.addColumn(family.getBytes(), column1.getBytes());
+                get.addColumn(family.getBytes(), column2.getBytes());
+                gets.add(get);
+            }
+            
+            hTable.batch(puts);
+            
+            for (int i = 0; i < 10; ++i) {
+                Result result = hTable.get(gets.get(i));
+                Assert(tableName, ()->Assert.assertEquals(2, result.size()));
+                for (Cell cell : result.rawCells()) {
+                    Assert(tableName, ()->Assert.assertEquals(timestamp, cell.getTimestamp()));
+                }
+                int finalI = i;
+                Assert(tableName, () -> Assert.assertTrue(secureCompare((column1 + value + timestamp + finalI).getBytes(), result.getValue(family.getBytes(), column1.getBytes()))));
+            }
+        }
+        
+        
         hTable.close();
     }
 
     public static void testMultiCFPutImpl(Map.Entry<String, List<String>> entry) throws Exception {
         String key = "putKey";
+        String column1 = "putColumn1";
+        String column2 = "putColumn2";
         String value = "value";
-        String column = "putColumn";
-        long timestamp = System.currentTimeMillis();
-
-        OHTableClient hTable = ObHTableTestUtil.newOHTableClient(entry.getKey());
+        OHTableClient hTable = ObHTableTestUtil.newOHTableClient(getTableName(entry.getKey()));
         hTable.init();
-        Put put = new Put(toBytes(key));
-        for (String tableName : entry.getValue()) {
-            String family = getColumnFamilyName(tableName);
-            put.add(family.getBytes(), column.getBytes(), timestamp, toBytes(value));
+        {
+            long currentTime = System.currentTimeMillis();
+            Put put = new Put(toBytes(key));
+            Get get = new Get(toBytes(key));
+            for (String tableName : entry.getValue()) {
+                String family = getColumnFamilyName(tableName);
+                put.add(family.getBytes(), column1.getBytes(), toBytes(column1 + value));
+                put.add(family.getBytes(), column2.getBytes(), toBytes(column2 + value));
+                get.addColumn(family.getBytes(), column1.getBytes());
+                get.addColumn(family.getBytes(), column2.getBytes());
+            }
+            hTable.put(put);
+            Result r = hTable.get(get);
+            Assert(entry.getValue(), ()->Assert.assertEquals(entry.getValue().size() * 2, r.size()));
+            long remoteTimestamp = r.rawCells()[0].getTimestamp();
+            Assert(entry.getValue(), ()->Assert.assertTrue(remoteTimestamp >= currentTime));
+            for (Cell cell : r.rawCells()) {
+                Assert(entry.getValue(), ()->Assert.assertEquals(remoteTimestamp, cell.getTimestamp()));
+            }
         }
-        hTable.put(put);
+
+        {
+            long timestamp = System.currentTimeMillis();
+            Put put = new Put(toBytes(key));
+            Get get = new Get(toBytes(key));
+            for (String tableName : entry.getValue()) {
+                String family = getColumnFamilyName(tableName);
+                put.add(family.getBytes(), column1.getBytes(), timestamp, toBytes(column1 + value));
+                put.add(family.getBytes(), column2.getBytes(), timestamp, toBytes(column2 + value));
+                get.addColumn(family.getBytes(), column1.getBytes());
+                get.addColumn(family.getBytes(), column2.getBytes());
+            }
+            
+            hTable.put(put);
+            Result r = hTable.get(get);
+            Assert(entry.getValue(), ()->Assert.assertEquals(entry.getValue().size() * 2, r.size()));
+            for (Cell cell : r.rawCells()) {
+                Assert(entry.getValue(), ()->Assert.assertEquals(timestamp, cell.getTimestamp()));
+            }
+            for (String tableName : entry.getValue()) {
+                String family = getColumnFamilyName(tableName);
+                Assert(entry.getValue(), () -> Assert.assertTrue(secureCompare((column1 + value).getBytes(), r.getValue(family.getBytes(), column1.getBytes()))));
+                Assert(entry.getValue(), () -> Assert.assertTrue(secureCompare((column2 + value).getBytes(), r.getValue(family.getBytes(), column2.getBytes()))));
+            }
+        }
+        
         hTable.close();
     }
 
+    public static void testMltiCFPutBatchImpl(Map.Entry<String, List<String>> entry) throws Exception {
+        String key = "putKey";
+        String column1 = "putColumn1";
+        String column2 = "putColumn2";
+        String value = "value";
+        OHTableClient hTable = ObHTableTestUtil.newOHTableClient(getTableName(entry.getKey()));
+        hTable.init();
+
+        {
+            long timestamp = System.currentTimeMillis();
+            List<Put> puts = new ArrayList<>();
+            Get get = new Get(toBytes(key));
+            for (String tableName : entry.getValue()) {
+                String family = getColumnFamilyName(tableName);
+                Put put = new Put(toBytes(key));
+                put.add(family.getBytes(), column1.getBytes(), timestamp, toBytes(column1 + value + timestamp));
+                put.add(family.getBytes(), column2.getBytes(), timestamp, toBytes(column2 + value + timestamp));
+                puts.add(put);
+                get.addColumn(family.getBytes(), column1.getBytes());
+                get.addColumn(family.getBytes(), column2.getBytes());
+            }
+            hTable.batch(puts);
+            Result result = hTable.get(get);
+            Assert(entry.getValue(), ()->Assert.assertEquals(entry.getValue().size() * 2, result.size()));
+            for (Cell cell : result.rawCells()) {
+                Assert(entry.getValue(), ()->Assert.assertEquals(timestamp, cell.getTimestamp()));
+            }
+            for (String tableName : entry.getValue()) {
+                String family = getColumnFamilyName(tableName);
+                Assert(entry.getValue(), () -> Assert.assertTrue(secureCompare((column1 + value + timestamp).getBytes(), result.getValue(family.getBytes(), column1.getBytes()))));
+                Assert(entry.getValue(), () -> Assert.assertTrue(secureCompare((column2 + value + timestamp).getBytes(), result.getValue(family.getBytes(), column2.getBytes()))));
+            }
+        }
+
+        {
+            long timestamp = System.currentTimeMillis();
+            List<Put> puts = new ArrayList<>();
+            List<Pair<Get,String>> gets = new ArrayList<>();
+            
+            for (String tableName : entry.getValue()) {
+                String family = getColumnFamilyName(tableName);
+                for (int i = 0; i < 10; ++i) {
+                    Put put = new Put(toBytes(key + i));
+                    put.add(family.getBytes(), column1.getBytes(), timestamp, toBytes(column1 + value + timestamp));
+                    put.add(family.getBytes(), column2.getBytes(), timestamp, toBytes(column2 + value + timestamp));
+                    puts.add(put);
+                    Get get = new Get(toBytes(key + i));
+                    get.addColumn(family.getBytes(), column1.getBytes());
+                    get.addColumn(family.getBytes(), column2.getBytes());
+                    gets.add(new Pair<>(get, family));
+                }
+            }
+            hTable.batch(puts);
+            for (int i = 0; i < 10; ++i) {
+                Result result = hTable.get(gets.get(i).getFirst());
+                Assert(entry.getValue(), () -> Assert.assertEquals(2, result.size()));
+                for (Cell cell : result.rawCells()) {
+                    Assert(entry.getValue(), () -> Assert.assertEquals(timestamp, cell.getTimestamp()));
+                }
+                int finalI = i;
+                Assert(entry.getValue(), () -> Assert.assertTrue(secureCompare((column1 + value + timestamp).getBytes(), result.getValue(gets.get(finalI).getSecond().getBytes(), column1.getBytes()))));
+                Assert(entry.getValue(), () -> Assert.assertTrue(secureCompare((column2 + value + timestamp).getBytes(), result.getValue(gets.get(finalI).getSecond().getBytes(), column2.getBytes()))));
+            }
+            
+        }
+        hTable.close();
+    }
+    
+    
     @Test
     public void testPut() throws Throwable {
         FOR_EACH(tableNames, OHTableSecondaryPartPutTest::testPutImpl);
     }
-
+    
+    @Test
+    public void testBatchPut() throws Throwable {
+        FOR_EACH(tableNames, OHTableSecondaryPartPutTest::testBatchPutImpl);
+    }
+    
     @Test
     public void testMultiCFPut() throws Throwable {
         FOR_EACH(group2tableNames, OHTableSecondaryPartPutTest::testMultiCFPutImpl);
+    }
+    
+    @Test
+    public void testMultiCFPutBatch() throws Throwable {
+        FOR_EACH(group2tableNames, OHTableSecondaryPartPutTest::testMltiCFPutBatchImpl);
     }
 
 }
