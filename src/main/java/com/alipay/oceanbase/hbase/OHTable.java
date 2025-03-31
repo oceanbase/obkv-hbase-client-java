50af7ab8bb625d5dd0acc1df4f0527e3346931b8
maochongxin <maochongxin@gmail.com>
2025-03-31 20:08:03 +0800

    fix

--- src/main/java/com/alipay/oceanbase/hbase/OHTable.java
+++ src/main/java/com/alipay/oceanbase/hbase/OHTable.java
@@ -1927,7 +1927,9 @@ public class OHTable implements HTableInterface {
         obTableQuery.setIndexName("PRIMARY");
         obTableQuery.sethTableFilter(filter);
         obTableQuery.addKeyRange(obNewRange);
-        obTableQuery.setScanRangeColumns("K", "Q", "T");
+        if (obTableClient.isOdpMode()) {
+            obTableQuery.setScanRangeColumns("K", "Q", "T");
+        }
         return obTableQuery;
     }
 
@@ -1954,7 +1956,9 @@ public class OHTable implements HTableInterface {
             : configuration.getLong(HConstants.HBASE_CLIENT_SCANNER_MAX_RESULT_SIZE_KEY,
                 HConstants.DEFAULT_HBASE_CLIENT_SCANNER_MAX_RESULT_SIZE));
         obTableQuery.setObKVParams(buildOBKVParams(scan));
-        obTableQuery.setScanRangeColumns("K", "Q", "T");
+        if (obTableClient.isOdpMode()) {
+            obTableQuery.setScanRangeColumns("K", "Q", "T");
+        }
         return obTableQuery;
     }
 
@@ -1980,7 +1984,9 @@ public class OHTable implements HTableInterface {
                 get.getTimeRange());
         }
         obTableQuery.setObKVParams(buildOBKVParams(get));
-        obTableQuery.setScanRangeColumns("K", "Q", "T");
+        if (obTableClient.isOdpMode()) {
+            obTableQuery.setScanRangeColumns("K", "Q", "T");
+        }
         return obTableQuery;
     }
 
